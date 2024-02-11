a3c6f0c27cc080138521bda7fd4687e15dbbb412
zkujun25 <109544556+zkujun25@users.noreply.github.com>
2024-02-11 02:25:31 +0100

    Add files via upload

---
+++ Vjezba1/Comparables/Bubble.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparables
+{
+    class Bubble
+    {
+        public static void Sort(Student[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+                for (int j = i + 1; j < array.Length; j++)
+                    if (array[j].CompareTo(array[i]) > 0)
+                    {
+                        Student temp = array[j];
+                        array[j] = array[i];
+                        array[i] = temp;
+                    }
+        }
+    }
+
+}
---
+++ Vjezba1/Comparables/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Comparables
+{ 
+    class Program
+    {
+        static void Main()
+        {
+            Student[] students = {
+                new("Ivo", 4.1),
+                new("Ana", 4.9),
+                new("Iva", 4.3),
+                new("Bob", 4.5),
+                new("Joe", 4.7)
+            };
+
+            foreach (Student student in students)
+            {
+                Console.WriteLine(student);
+            }
+
+            Console.WriteLine("----------");
+
+            Bubble.Sort(students);
+
+            foreach (Student student in students)
+            {
+                Console.WriteLine(student);
+            }
+        }
+
+    }
+}
